24ff519a4df03027f812320e0f1438b20249956c
leomezza <leomezza@gmail.com>
2023-07-16 01:02:04 -0300

    formatei 6-11

--- 6-11.cities.py
+++ 6-11.cities.py
@@ -1,11 +1,23 @@
 cities = {
-    "Indaiatuba": {"country": "Brazil", "population": "260.000", "fact": "Show de bola",},
-    "Austin": {"country": "USA", "population": "2.000.000", "fact": "Paraíso",},
-    "Vinhedo": {"country": "Brazil", "population": "80.000", "fact": "Calmo",},
+    "Indaiatuba": {
+        "country": "Brazil",
+        "population": "260.000",
+        "fact": "Show de bola",
+    },
+    "Austin": {
+        "country": "USA",
+        "population": "2.000.000",
+        "fact": "Paraíso",
+    },
+    "Vinhedo": {
+        "country": "Brazil",
+        "population": "80.000",
+        "fact": "Calmo",
+    },
 }
 
 for city, info in cities.items():
-    print(f'\nCity: {city}')
+    print(f"\nCity: {city}")
     print(f'\tCountry: {info["country"]}')
     print(f'\tPopulation: {info["population"]}')
     print(f'\tFact: {info["fact"]}')
