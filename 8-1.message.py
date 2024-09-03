e4699010198a4ab07a0d54fd210508e05aaf23a1
leomezza <leomezza@gmail.com>
2024-09-03 13:58:10 -0300

    Exercise 8-1

---
+++ 8-1.message.py
@@ -0,0 +1,5 @@
+def display_message():
+    """Tells my learning of functions"""
+    print("Hello everyone, I'm learning about functions, their parameters and arguments, in this chapter")
+
+display_message()
